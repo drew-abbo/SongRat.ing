3190f408f794ab8914af5784d6f1b50d9a69b3a4
drew-abbo <drew.a.abbo@gmail.com>
2024-10-24 21:43:38 -0400

    removed most database scripts, keeping only 'connect.sh' script

--- database/connect.sh
+++ database/connect.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# setup env for database connection
-cd "$(dirname "$0")" && source ./db_script_setup.sh && cd - > /dev/null
+# setup env for database connection by reading '.env' file
+cd "$(dirname "$0")"
+export $(grep -v '^\s*#' ../.env | xargs)
+export PGPASSWORD=$POSTGRES_PASSWORD
+cd - > /dev/null
 
 # open interactive shell
 docker exec -it postgres psql -U "$POSTGRES_USER" -d song_rating_db
--- database/db_script_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# load .env
-export $(grep -v '^\s*#' ../.env | xargs)
-
-# export db password to avoid password prompt
-export PGPASSWORD=$POSTGRES_PASSWORD
