dc435e052e4b81936eabe7f17651baed7a141fe2
drew-abbo <drew.a.abbo@gmail.com>
2024-10-26 12:47:55 -0400

    all endpoints now respond with 'hello world' when input parameters and body are valid

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,136 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
-import joi from "joi";
 
-import * as code from "./generate_code";
+import * as code from "./code";
+import { validateBodyOnSchemaMiddleware } from "./schemas";
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// /game
+
+app.post(
+  "/game/new",
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(201).json({ hello: "world" });
+  }
+);
+
+app.get(
+  "/game/peek/:invite_code",
+  code.createValidatorMiddleware(code.Kind.INVITE),
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(200).json({ hello: "world" });
+  }
+);
+
+app.post(
+  "/game/join/:invite_code",
+  code.createValidatorMiddleware(code.Kind.INVITE),
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(201).json({ hello: "world" });
+  }
+);
+
+// /admin
+
+app.get(
+  "/admin/review/:master_code",
+  code.createValidatorMiddleware(code.Kind.ADMIN),
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(200).json({ hello: "world" });
+  }
+);
+
+app.post(
+  "/admin/begin/:master_code",
+  code.createValidatorMiddleware(code.Kind.ADMIN),
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(201).json({ hello: "world" });
+  }
+);
+
+app.post(
+  "/admin/end/:master_code",
+  code.createValidatorMiddleware(code.Kind.ADMIN),
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(201).json({ hello: "world" });
+  }
+);
+
+app.post(
+  "/admin/remove_player/:master_code",
+  code.createValidatorMiddleware(code.Kind.ADMIN),
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(201).json({ hello: "world" });
+  }
+);
+
+// /player
+
+app.get(
+  "/player/review/:player_code",
+  code.createValidatorMiddleware(code.Kind.PLAYER),
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(200).json({ hello: "world" });
+  }
+);
+
+app.post(
+  "/player/add_song/:player_code",
+  code.createValidatorMiddleware(code.Kind.PLAYER),
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(201).json({ hello: "world" });
+  }
+);
+
+app.post(
+  "/player/remove_song/:player_code",
+  code.createValidatorMiddleware(code.Kind.PLAYER),
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(201).json({ hello: "world" });
+  }
+);
+
+app.post(
+  "/player/replace_song/:player_code",
+  code.createValidatorMiddleware(code.Kind.PLAYER),
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(201).json({ hello: "world" });
+  }
+);
+
+app.post(
+  "/player/change_playlist_link/:player_code",
+  code.createValidatorMiddleware(code.Kind.PLAYER),
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(201).json({ hello: "world" });
+  }
+);
+
+app.post(
+  "/player/rate_song/:player_code",
+  code.createValidatorMiddleware(code.Kind.PLAYER),
+  validateBodyOnSchemaMiddleware,
+  (req: Request, res: Response) => {
+    return res.status(201).json({ hello: "world" });
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Backend listening on http://localhost:${PORT}`);
