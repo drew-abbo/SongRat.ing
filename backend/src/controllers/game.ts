07fdb8cd38f739bdea9aa0286bc7fe45d6cdca9a
drew-abbo <drew.a.abbo@gmail.com>
2024-10-26 18:42:45 -0400

    implemented createGame operation

--- backend/src/controllers/game.ts
+++ backend/src/controllers/game.ts
@@ -1,9 +1,47 @@
 import { Request, Response } from "express";
 
+import * as code from "../middleware/code";
+import basic500 from "../middleware/basic500";
+import db from "../db";
+
 const notImplemented = (req: Request, res: Response) => {
   res.status(501).json({ message: "Not implemented" });
 };
 
-export const createGame = notImplemented;
+export async function createGame(req: Request, res: Response) {
+  const adminCode = code.generate(code.Kind.ADMIN);
+  const inviteCode = code.generate(code.Kind.INVITE);
+
+  try {
+    await db.query(
+      `INSERT INTO games (
+        admin_code,
+        game_name,
+        game_description,
+        min_songs_per_playlist,
+        max_songs_per_playlist,
+        require_playlist_link,
+        invite_code)
+      VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+      [
+        adminCode,
+        req.body.game_name,
+        req.body.game_description,
+        req.body.min_songs_per_playlist,
+        req.body.max_songs_per_playlist,
+        req.body.require_playlist_link,
+        inviteCode,
+      ]
+    );
+  } catch (err) {
+    return basic500(res, err);
+  }
+
+  return res.status(201).json({
+    admin_code: adminCode,
+    inviteCode: inviteCode,
+  });
+}
+
 export const peekGameInfo = notImplemented;
 export const joinGame = notImplemented;
