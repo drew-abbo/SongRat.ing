188158e9a431933110720de3f2c5c7ffd9a4d5dc
drew-abbo <drew.a.abbo@gmail.com>
2024-10-27 23:59:35 -0400

    implemented adminBeginGame operation

--- backend/src/controllers/admin.ts
+++ backend/src/controllers/admin.ts
@@ -1,12 +1,36 @@
 import { Request, Response } from "express";
 
-import * as code from "../middleware/code";
 import basic500 from "../middleware/basic500";
 import db from "../db";
 
 const notImplemented = (req: Request, res: Response) => {
   res.status(501).json({ message: "Not implemented" });
 };
+
+/**
+ * Returns whether a game exists given an admin code.
+ * @param adminCode The admin code to check the existence of.
+ * @returns Whether or not the admin code is valid.
+ *
+ * @throws Any error that happens as a result of a database read failure.
+ */
+async function gameExists(adminCode: string): Promise<boolean> {
+  return (
+    await db.query(
+      `SELECT EXISTS (
+          SELECT 1
+          FROM games
+          WHERE admin_code = $1
+        ) AS game_exists`,
+      [adminCode]
+    )
+  ).rows[0].game_exists;
+}
+
+/** Returns a 404 with the message "Unknown admin code". */
+function unknownAdminCode(res: Response) {
+  return res.status(404).json({ message: "Unknown admin code" });
+}
 
 export async function adminReviewGame(req: Request, res: Response) {
   type GameInfo = {
@@ -116,7 +140,7 @@ export async function adminReviewGame(req: Request, res: Response) {
     const ratings: Ratings = ratingsQueryResult.rows;
 
     if (!gameInfo) {
-      return res.status(404).json({ message: "Unknown admin code" });
+      return unknownAdminCode(res);
     }
 
     gameInfo.players = players;
@@ -129,6 +153,30 @@ export async function adminReviewGame(req: Request, res: Response) {
   }
 }
 
-export const adminBeginGame = notImplemented;
+export async function adminBeginGame(req: Request, res: Response) {
+  try {
+    db.query(
+      `UPDATE games
+      SET game_status = 'active', invite_code = NULL
+      WHERE admin_code = $1 AND game_status = 'waiting_for_players'`,
+      [req.params.admin_code]
+    );
+    return res.status(201).json({ message: "Game begun successfully" });
+  } catch (err) {
+    // if it failed see if it failed because the admin code doesn't exist or
+    // because the game status isn't 'waiting_for_players'
+    try {
+      if (await gameExists(req.params.admin_code)) {
+        return res.status(409).json({
+          message: "Can't begin an active or finished game",
+        });
+      }
+      return unknownAdminCode(res);
+    } catch (err) {
+      return basic500(res, err);
+    }
+  }
+}
+
 export const adminEndGame = notImplemented;
 export const adminRemovePlayer = notImplemented;
