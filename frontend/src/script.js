be8ff8e067d6d00132592ecbed7ef4d939906056
drew-abbo <drew.a.abbo@gmail.com>
2024-11-02 16:46:12 -0400

    created 404 page, got color scheme, and set up vanilla js component system (w/ nav bar)

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,1 +1,16 @@
-console.log("Hello world from JS!");
+/**
+ * An object with methods for creating components.
+ */
+const components = Object.freeze({
+  // top navigation bar
+  top_nav_bar: () => {
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `<div class="top-nav-bar">
+        <a href="/">Home</a>
+        <a href="https://github.com/drew-abbo/song_rating_game">GitHub</a>
+        <a href="foo">foo</a>
+      </div>`
+    );
+  },
+});
