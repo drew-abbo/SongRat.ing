68e362c8102a04690d9b74bf312217f6934754ce
drew-abbo <drew.a.abbo@gmail.com>
2024-11-10 00:58:40 -0500

    updated patch script

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+git fetch origin
+
+if [[ "$(git rev-parse main)" == "$(git rev-parse origin/main)" ]]; then
+  echo "No changes to patch."
+  exit 0
+fi
+
 docker-compose stop
 
-git pull
+git pull || docker-compose up -d && echo "Patch failed to be applied."
 
 docker-compose up --build -d
+echo "Patch applied."
